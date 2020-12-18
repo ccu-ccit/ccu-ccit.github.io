d47842b8cfe3f7be397e7986645011d9236a1569
Stan Su <sstan@fortinet.com>
2020-12-18 15:36:32 +0800

    change the request api to https

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import { VueAxios } from './axios'
 const service = axios.create({
-	baseURL: "http://ccit.herokuapp.com",
+	baseURL: "https://ccit.herokuapp.com",
     timeout: 30000,
 	withCredentials: false
 })
